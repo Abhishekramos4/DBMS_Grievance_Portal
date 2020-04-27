519990de483e05de3c6c6e99af4f696c1501ab49
Abhishek4 <abhishekkekane@gmail.com>
2020-04-27 07:55:49 +0530

    Added duration filter and In Progress for admin

--- server.js
+++ server.js
@@ -15,24 +15,24 @@ var lnamedoc;
 var password;
 var repassword;
 //Date Formatting
-function formatDate() {
-    var date = new Date();
-    var month_names = ["Jan", "Feb", "Mar",
-        "Apr", "May", "Jun",
-        "Jul", "Aug", "Sep",
-        "Oct", "Nov", "Dec"];
-    var month_digit = ["01", "02", "03",
-        "04", "05", "06",
-        "07", "08", "09",
-        "10", "11", "12"];
+// function formatDate() {
+//     var date = new Date();
+//     var month_names = ["Jan", "Feb", "Mar",
+//         "Apr", "May", "Jun",
+//         "Jul", "Aug", "Sep",
+//         "Oct", "Nov", "Dec"];
+//     var month_digit = ["01", "02", "03",
+//         "04", "05", "06",
+//         "07", "08", "09",
+//         "10", "11", "12"];
 
 
-    var day = date.getDate();
-    var month_index = date.getMonth();
-    var year = date.getFullYear();
+//     var day = date.getDate();
+//     var month_index = date.getMonth();
+//     var year = date.getFullYear();
 
-    return "" + day + "-" + month_digit[month_index] + "-" + year;
-}
+//     return "" + day + "-" + month_digit[month_index] + "-" + year;
+// }
 
 
 mongoose.connect('mongodb://localhost:27017/GrievanceDB', { useNewUrlParser: true, useUnifiedTopology: true });
@@ -61,14 +61,15 @@ const RegisterSchema = {
 
 //Schema for Complaint 
 const ComplaintSchema = {
-    dateIssued: { type: String, },
+    dateIssued: { type: Date, },
     location: { type: String },
     section: { type: String },
     description: { type: String },
-    dateResolved: { type: String },
+    dateResolved: { type: Date },
     isSolved: { type: Boolean },
     adminFeedBack: { type: String },
     studentID: { type: Number },
+    inProgress: { type: Boolean },
 
 }
 
@@ -146,21 +147,22 @@ app.route('/profile-register')
     .post(function (req, res) {
 
         var lodgedComplaint = new complaintModel({
-            dateIssued: formatDate(),
+            dateIssued: new Date(),
             location: req.body.location,
             section: req.body.section,
             description: req.body.description,
             dateResolved: null,
             isSolved: false,
             studentID: collegeIDdoc,
-            adminFeedBack: null
+            adminFeedBack: null,
+            inProgress: false
         })
         lodgedComplaint.save(function (err) {
             if (err) {
                 console.log(err);
             }
             else {
-                res.render('submitsuccess',{ name: fnamedoc + " " + lnamedoc, collegeID: collegeIDdoc })
+                res.render('submitsuccess', { name: fnamedoc + " " + lnamedoc, collegeID: collegeIDdoc })
             }
         });
     });
@@ -193,20 +195,61 @@ app.get('/profile-solved', function (req, res) {
 });
 
 //Sending Json route  (Sorting)    PENDING
-app.route('/complaints-pending/:location/:section')
+app.route('/complaints-pending/:location/:section/:duration')
     .get(function (req, res) {
         var location = req.params.location;
         var section = req.params.section;
+        var duration = req.params.duration;
         console.log(location);
         console.log(section);
         var filter = {};
-        if (location == "All") { filter = { isSolved: false }; }
+        if (duration == "month") {
+            var today = new Date();
+            var deadline = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+            filter = {
+                dateIssued: {
+                    $gte: new Date(deadline.toISOString()),
+                    $lt: new Date(today.toISOString())
+                }
+            }
+        }
+        else if (duration == "week") {
+            var today = new Date();
+            var deadline = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+            filter = {
+                dateIssued: {
+                    $gte: new Date(deadline.toISOString()),
+                    $lt: new Date(today.toISOString())
+                }
+            }
+        }
+        else if (duration = 'today') {
+            var today = new Date();
+            var deadline = today.setHours(0, 0, 0, 0);
+            var formatDeadline = new Date(deadline);
+            filter = {
+                dateIssued: {
+                    $gte: new Date(formatDeadline.toISOString()),
+                    $lt: new Date(today.toISOString())
+                }
+            }
+
+        }
+        if (location == "All") {
+            filter.isSolved = false
+            // filter = { isSolved: false }; 
+        }
         else if (section == "All") {
-            filter = { isSolved: false, location: location };
+            // filter = { isSolved: false, location: location };
+            filter.isSolved = false;
+            filter.location = location;
 
         }
         else {
-            filter = { isSolved: false, location: location, section: section };
+            // filter = { isSolved: false, location: location, section: section };
+            filter.isSolved = false;
+            filter.location = location;
+            filter.section = section;
         }
 
         complaintModel.find(filter, function (err, docs) {
@@ -220,21 +263,63 @@ app.route('/complaints-pending/:location/:section')
 
 
 //Sending Json route  (Sorting)            SOLVED 
-app.route('/complaints-solved/:location/:section')
+app.route('/complaints-solved/:location/:section/:duration')
     .get(function (req, res) {
         var location = req.params.location;
         var section = req.params.section;
+        var duration = req.params.duration;
         console.log(location);
         console.log(section);
         var filter = {};
-        if (location == "All") { filter = { isSolved: true }; }
+        if (duration == "month") {
+            var today = new Date();
+            var deadline = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+            filter = {
+                dateIssued: {
+                    $gte: new Date(deadline.toISOString()),
+                    $lt: new Date(today.toISOString())
+                }
+            }
+        }
+        else if (duration == "week") {
+            var today = new Date();
+            var deadline = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+            filter = {
+                dateIssued: {
+                    $gte: new Date(deadline.toISOString()),
+                    $lt: new Date(today.toISOString())
+                }
+            }
+        }
+        else if (duration == 'today') {
+            var today = new Date();
+            var deadline = today.setHours(0, 0, 0, 0);
+            var formatDeadline = new Date(deadline);
+            filter = {
+                dateIssued: {
+                    $gte: new Date(formatDeadline.toISOString()),
+                    $lt: new Date(today.toISOString())
+                }
+            }
+
+        }
+        if (location == "All") {
+            filter.isSolved = true;
+            // filter = { isSolved: false }; 
+        }
         else if (section == "All") {
-            filter = { isSolved: true, location: location };
+            // filter = { isSolved: false, location: location };
+            filter.isSolved = true;
+            filter.location = location;
 
         }
         else {
-            filter = { isSolved: true, location: location, section: section };
+            // filter = { isSolved: false, location: location, section: section };
+            filter.isSolved = true;
+            filter.location = location;
+            filter.section = section;
         }
+
 
         complaintModel.find(filter, function (err, docs) {
             if (err) { console.log(err) }
@@ -275,20 +360,64 @@ app.get('/profile-mycomplaints-solved', function (req, res) {
     );
 });
 //Sending Json route  (Sorting)    PENDING
-app.route('/complaints-pending-my/:location/:section')
+app.route('/complaints-pending-my/:location/:section/:duration')
     .get(function (req, res) {
         var location = req.params.location;
         var section = req.params.section;
+        var duration = req.params.duration;
         console.log(location);
         console.log(section);
         var filter = {};
-        if (location == "All") { filter = { isSolved: false, studentID: collegeIDdoc }; }
+        if (duration == "month") {
+            var today = new Date();
+            var deadline = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+            filter = {
+                dateIssued: {
+                    $gte: new Date(deadline.toISOString()),
+                    $lt: new Date(today.toISOString())
+                }
+            }
+        }
+        else if (duration == "week") {
+            var today = new Date();
+            var deadline = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+            filter = {
+                dateIssued: {
+                    $gte: new Date(deadline.toISOString()),
+                    $lt: new Date(today.toISOString())
+                }
+            }
+        }
+        else if (duration == 'today') {
+            var today = new Date();
+            var deadline = today.setHours(0, 0, 0, 0);
+            var formatDeadline = new Date(deadline);
+            filter = {
+                dateIssued: {
+                    $gte: new Date(formatDeadline.toISOString()),
+                    $lt: new Date(today.toISOString())
+                }
+            }
+
+        }
+        if (location == "All") {
+            filter.isSolved = false;
+            filter.collegeID = collegeIDdoc;
+            // filter = { isSolved: false }; 
+        }
         else if (section == "All") {
-            filter = { isSolved: false, location: location, studentID: collegeIDdoc };
+            // filter = { isSolved: false, location: location };
+            filter.isSolved = false;
+            filter.location = location;
+            filter.collegeID = collegeIDdoc;
 
         }
         else {
-            filter = { isSolved: false, location: location, section: section, studentID };
+            // filter = { isSolved: false, location: location, section: section };
+            filter.isSolved = false;
+            filter.location = location;
+            filter.section = section;
+            filter.collegeID = collegeIDdoc;
         }
 
         complaintModel.find(filter, function (err, docs) {
@@ -302,21 +431,66 @@ app.route('/complaints-pending-my/:location/:section')
 
 
 //Sending Json route  (Sorting)            SOLVED 
-app.route('/complaints-solved-my/:location/:section')
+app.route('/complaints-solved-my/:location/:section/:duration')
     .get(function (req, res) {
         var location = req.params.location;
         var section = req.params.section;
+        var duration = req.params.duration;
         console.log(location);
         console.log(section);
         var filter = {};
-        if (location == "All") { filter = { isSolved: true, studentID: collegeIDdoc }; }
+        if (duration == "month") {
+            var today = new Date();
+            var deadline = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+            filter = {
+                dateIssued: {
+                    $gte: new Date(deadline.toISOString()),
+                    $lt: new Date(today.toISOString())
+                }
+            }
+        }
+        else if (duration == "week") {
+            var today = new Date();
+            var deadline = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+            filter = {
+                dateIssued: {
+                    $gte: new Date(deadline.toISOString()),
+                    $lt: new Date(today.toISOString())
+                }
+            }
+        }
+        else if (duration == "week") {
+            var today = new Date();
+            var deadline = today.setHours(0, 0, 0, 0);
+            var formatDeadline = new Date(deadline);
+            filter = {
+                dateIssued: {
+                    $gte: new Date(formatDeadline.toISOString()),
+                    $lt: new Date(today.toISOString())
+                }
+            }
+
+        }
+        if (location == "All") {
+            filter.isSolved = true;
+            filter.collegeID = collegeIDdoc;
+            // filter = { isSolved: false }; 
+        }
         else if (section == "All") {
-            filter = { isSolved: true, location: location, studentID: collegeIDdoc };
+            // filter = { isSolved: false, location: location };
+            filter.isSolved = true;
+            filter.location = location;
+            filter.collegeID = collegeIDdoc;
 
         }
         else {
-            filter = { isSolved: true, location: location, section: section, studentID: collegeIDdoc };
+            // filter = { isSolved: false, location: location, section: section };
+            filter.isSolved = true;
+            filter.location = location;
+            filter.section = section;
+            filter.collegeID = collegeIDdoc;
         }
+
 
         complaintModel.find(filter, function (err, docs) {
             if (err) { console.log(err) }
@@ -330,7 +504,26 @@ app.route('/complaints-solved-my/:location/:section')
 
 //----------------------------------------- ADMIN-----------------------------------------------//
 
-//Solved Complaint Sent By ADMIN
+//Complaint Sent By ADMIN to InProgress
+app.route('/inprogress-complaints')
+    .post(function (req, res) {
+        var id = req.body.id;
+        var inProgress = req.body.inProgress;
+
+        console.log(id);
+        console.log(isSolved);
+        complaintModel.findOneAndUpdate({ _id: id, inProgress: false, }, { inProgress: true }, function (err) {
+            if (err) {
+                console.log(err);
+            }
+            else { console.log("Successfully sent the complaint ") }
+
+        });
+
+
+    });
+
+
 app.route('/solved-complaints')
     .post(function (req, res) {
         var id = req.body.id;
@@ -338,7 +531,7 @@ app.route('/solved-complaints')
         var feedback = req.body.feedback;
         console.log(id);
         console.log(isSolved);
-        complaintModel.findOneAndUpdate({ _id: id, isSolved: false, }, { isSolved: true, dateResolved: formatDate(), adminFeedBack: feedback }, function (err) {
+        complaintModel.findOneAndUpdate({ _id: id, isSolved: false, }, { isSolved: true, dateResolved: new Date(), adminFeedBack: feedback }, function (err) {
             if (err) {
                 console.log(err);
             }
@@ -350,22 +543,174 @@ app.route('/solved-complaints')
     });
 
 
-
 //GET COMPLAINTS
-app.get('/admin', function (req, res) {
-    complaintModel.find({ isSolved: false }, function (err, docs) {
-        if (err) { console.log(err) }
+// app.get('/admin', function (req, res) {
+
+
+//     complaintModel.find({ isSolved: false }, function (err, docs) {
+//         if (err) { console.log(err) }
+//         else {
+//             res.send(docs);
+//         }
+//     });
+// });
+
+//GET  COMPLAINTS to In Progress
+app.get('/admin/:location/:section/:duration', function (req, res) {
+    var location = req.params.location;
+    var section = req.params.section;
+    var duration = req.params.duration;
+    console.log(location);
+    console.log(section);
+    var filter = {};
+    if (duration == "month") {
+        var today = new Date();
+        var deadline = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+        filter = {
+            dateIssued: {
+                $gte: new Date(deadline.toISOString()),
+                $lt: new Date(today.toISOString())
+            }
+        }
+    }
+    else if (duration == "week") {
+        var today = new Date();
+        var deadline = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+        filter = {
+            dateIssued: {
+                $gte: new Date(deadline.toISOString()),
+                $lt: new Date(today.toISOString())
+            }
+        }
+    }
+    else if (duration == 'today') {
+        var today = new Date();
+        var deadline = today.setHours(0, 0, 0, 0);
+        var formatDeadline = new Date(deadline);
+        filter = {
+            dateIssued: {
+                $gte: new Date(formatDeadline.toISOString()),
+                $lt: new Date(today.toISOString())
+            }
+        }
+
+    }
+    if (location == "All") {
+        filter.isSolved = false;
+        filter.inProgress = false;
+        // filter = { isSolved: false }; 
+    }
+    else if (section == "All") {
+        // filter = { isSolved: false, location: location };
+        filter.isSolved = false;
+        filter.inProgress = false;
+        filter.location = location;
+
+    }
+    else {
+        // filter = { isSolved: false, location: location, section: section };
+        filter.isSolved = false;
+        filter.inProgress = false;
+        filter.location = location;
+        filter.section = section;
+    }
+
+    complaintModel.find(filter, function (err, docs) {
+        if (err) {
+            console.log(err);
+
+        }
         else {
             res.send(docs);
         }
+
+
     });
 });
+
+//GET  COMPLAINTS to solved
+app.get('/admin-to-solve/:location/:section/:duration', function (req, res) {
+    var location = req.params.location;
+    var section = req.params.section;
+    var duration = req.params.duration;
+    console.log(location);
+    console.log(section);
+    var filter = {};
+    if (duration == "month") {
+        var today = new Date();
+        var deadline = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+        filter = {
+            dateIssued: {
+                $gte: new Date(deadline.toISOString()),
+                $lt: new Date(today.toISOString())
+            }
+        }
+    }
+    else if (duration == "week") {
+        var today = new Date();
+        var deadline = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+        filter = {
+            dateIssued: {
+                $gte: new Date(deadline.toISOString()),
+                $lt: new Date(today.toISOString())
+            }
+        }
+    }
+    else if (duration == 'today') {
+        var today = new Date();
+        var deadline = today.setHours(0, 0, 0, 0);
+        var formatDeadline = new Date(deadline);
+        filter = {
+            dateIssued: {
+                $gte: new Date(formatDeadline.toISOString()),
+                $lt: new Date(today.toISOString())
+            }
+        }
+
+    }
+    if (location == "All") {
+        filter.isSolved = false;
+        filter.inProgress = true;
+        // filter = { isSolved: false }; 
+    }
+    else if (section == "All") {
+        // filter = { isSolved: false, location: location };
+        filter.isSolved = false;
+        filter.inProgress = true;
+        filter.location = location;
+
+    }
+    else {
+        // filter = { isSolved: false, location: location, section: section };
+        filter.isSolved = false;
+        filter.inProgress = true;
+        filter.location = location;
+        filter.section = section;
+    }
+
+    complaintModel.find(filter, function (err, docs) {
+        if (err) {
+            console.log(err);
+
+        }
+        else {
+            res.send(docs);
+        }
+
+
+    });
+});
+
+
 //ADMIN PAGE
 app.get('/admin-page', function (req, res) {
     res.render('admin');
 
 });
-
+//ADMIN IN PROGRESS PAGE
+app.get('/admin-page-progress', function (req, res) {
+    res.render('admininprogress');
+})
 
 
 
